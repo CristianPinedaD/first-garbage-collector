0ac7a9ae67b0fa2b9f1c2751845dcbb95014e50c
CristianPinedaD <109752965+CristianPinedaD@users.noreply.github.com>
2025-02-04 12:16:26 -0500

    vm push-pop functionality

--- main.c
+++ main.c
@@ -26,3 +26,18 @@ typedef struct {
 	int stackSize;
 } VM; 
 
+VM* newVM() {
+	VM *vm = malloc(sizeof(VM));
+	vm->stackSize = 0;
+	return vm; 
+}
+
+void push(VM* vm, Object* value) {
+	assert(vm->stackSize < STACK_MAX, "Stack overflow!");
+	vm->stack[vm->stackSize++] = value; 
+}
+
+Object* pop(VM* vm) {
+	assert(vm->stackSize > 0; "Stack underflow!");
+	return vm->stack[--vm->stackSize];
+}
